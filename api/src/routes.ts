d37811f0dbcd5663e2cf05176820680721963cfe
Marioto <henrique.marioto@outlook.com>
2023-04-19 14:18:12 -0300

    feat: comments on confession

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import ConfessionController from './controllers/Confession.controller'
 import { confessionSchema } from './validatorSchemas/confession.schema'
 import validatorMiddleware from './middlewares/validator.middleware'
+import CommentController from './controllers/Comment.controller'
+import { commentSchema } from './validatorSchemas/comment.schema'
 
 class Routes {
   routes: Router
@@ -15,6 +17,7 @@ class Routes {
     this.routes.get('/is-alive', (req, res) => res.json({ message: 'The Confession is alive' }))
 
     this.routes.post('/create-confession', confessionSchema, validatorMiddleware, ConfessionController.create)
+    this.routes.post('/comment-on-confession', commentSchema, validatorMiddleware, CommentController.create)
   }
 }
 
